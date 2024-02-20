c455c3e6c4b61f0184099a80d75825afd31ab716
dota <76216654+chuanqidota@users.noreply.github.com>
2024-02-20 11:27:42 +0800

    close chan

--- app/ws/view/view.go
+++ app/ws/view/view.go
@@ -103,7 +103,9 @@ func (w wsHandle) Handler(c *gin.Context) {
 
 	// 核心交互
 	quitChan := make(chan bool, 4)
+	defer close(quitChan)
 	esDataChan := make(chan []byte)
+	defer close(esDataChan)
 	go t.ReceiveWsMsg(conn, quitChan, key, startTime, record) // ws > terminal
 	go t.WriteWsMsg(conn, quitChan, esDataChan)               // terminal > ws
 	go t.WriteEsData(quitChan, key, startTime, record, esDataChan)
