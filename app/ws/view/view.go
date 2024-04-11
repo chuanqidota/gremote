e81cba8fd58aa30c9116788d1e64f70c6b24a3a4
chuanqidota <chuanqidota@126.com>
2024-04-11 22:14:55 +0800

    chan 大小

--- app/ws/view/view.go
+++ app/ws/view/view.go
@@ -108,7 +108,7 @@ func (w wsHandle) Handler(c *gin.Context) {
 	// 核心交互
 	quitChan := make(chan bool, 4)
 	defer close(quitChan)
-	esDataChan := make(chan []byte, 1024*1024)
+	esDataChan := make(chan []byte, 1024*1024*1024)
 	defer close(esDataChan)
 	go t.ReceiveWsMsg(conn, quitChan, key, startTime, record)      // ws > terminal
 	go t.WriteWsMsg(conn, quitChan, esDataChan)                    // terminal > ws & chan
